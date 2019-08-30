5f90111f4de4222e198b2446ffc595902b4fbbf4
sriramavadlamani <40035962+sriramavadlamani@users.noreply.github.com>
2019-08-29 19:46:55 -0700

    Add files via upload

---
+++ NumberToWordConverter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NumberToWordConverter.Startup))]
+namespace NumberToWordConverter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
